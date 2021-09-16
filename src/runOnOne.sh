fcd4c48e059d38f151b29a47f61ed837ef942c34
Ashton Omdahl <aomdahl@gmail.com>
2021-09-16 14:28:28 -0400

    quicker scripts for doing analysis on individual things

--- src/runOnOne.sh
+++ src/runOnOne.sh
@@ -2,10 +2,9 @@ set -e
 ml gcc/5.5.0
 ml R
 cd /work-zfs/abattle4/ashton/snp_networks/scratch/ldsc_all_traits
-for fact in `cat ${1}`; do
     T="/work-zfs/abattle4/ashton/snp_networks/custom_l1_factorization/"
-    echo $fact
-    mkdir -p results/${fact}
+fact=$1    
+mkdir -p results/${fact}
   Rscript src/factAssessment.R --factors ${T}/factorization_data/${fact}.factors.txt  \
     --output ./results/${fact} \
     --ldsc_reference ldsc_results/seed2_thres0.9_h2-0.1/ \
